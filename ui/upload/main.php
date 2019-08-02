41f773d50e9e5a5667d425c94db244d222be6a11
mukorojeffrey <mukorojeffreyoghenevwegba@gmail.com>
2019-08-02 18:12:47 +0100

    updated front-end for upload page

--- ui/upload/main.php
+++ ui/upload/main.php
@@ -1,3 +1,68 @@
-<h1>
-    Welcome to your new page. Open <b> <?php echo UI.$page_link."/main.php";?> </b> to edit
-</h1>
+<form method="post" action="app/upload/main">
+
+	<p>
+		<label>select picture:</label>
+		<input type="file" name="picture">
+	</p>
+
+    <p>
+		<label> Title:</label>
+		<input type="text" name="title">
+	</p>
+
+   <p>
+	    <label> Price:</label>
+	    <input type="text" name="price">
+   </p>
+
+   <p>
+       <label> All categories:</label>
+       <select name="category">
+	   	<option>Antiques</option>
+	   	<option>Art</option>
+		<option>Baby</option>
+		<option>Books</option>
+		<option>Business &amp; Industrial</option>
+		<option>Cameras &amp; Photo</option>
+		<option>Cell Phones &amp; Accessories</option>
+		<option>Clothing, Shoes &amp; Accessories</option>
+		<option>Coins &amp; Paper Money</option><option>Collectibles</option>
+		<option>Computers/Tablets &amp; Networking</option>
+		<option>Consumer Electronics</option>
+		<option>Crafts</option>
+		<option>Dolls &amp; Bears</option>
+		<option>DVDs &amp; Movies</option>
+		<option>eBay Motors</option>
+		<option>Entertainment Memorabilia
+		</option>
+		<option>Gift Cards &amp; Coupons
+		</option>
+		<option>Health &amp; Beauty</option>
+		<option>Home &amp; Garden</option>
+		<option>Jewelry &amp; Watches</option>
+		<option>Music</option>
+		<option>Musical Instruments &amp; Gear</option>
+		<option>Pet Supplies</option>
+		<option>Pottery &amp; Glass</option>
+		<option>Real Estate</option>
+		<option>Specialty Services</option>
+		<option>Sporting Goods</option>
+		<option>Sports Mem, Cards &amp; Fan Shop</option>
+		<option>Stamps</option>
+		<option>Tickets &amp; Experiences</option>
+		<option>Toys &amp; Hobbies</option>
+		<option>Travel</option>
+		<option>Video Games &amp; Consoles
+		</option>
+	  </select>
+	</p>
+
+	 <p>
+		  <label> details:</label>
+		  <textarea name="details"></textarea>
+	 </p>
+
+	  <p>
+	      <input type="Submit" value="Add product">
+	  </p>
+   </form>
