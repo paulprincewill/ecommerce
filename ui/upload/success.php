52bd0c60f20ccb02898e5c73f957b766a0837ebc
mukorojeffrey <mukorojeffreyoghenevwegba@gmail.com>
2019-08-02 18:30:58 +0100

    i created a success page

---
+++ ui/upload/success.php
@@ -0,0 +1,13 @@
+<div> 
+	<p> 
+	 Your product has been uploaded successfully
+	</p> 
+
+		<input type="text">
+		<a href="">View product</a>
+		<a href=""> Add product</a>
+	    <a href=""> See all product</a>
+
+
+
+</div>
