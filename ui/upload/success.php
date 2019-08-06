394b31c486284a522e843baa06e6b0b4814aa2c6
mukorojeffrey <mukorojeffreyoghenevwegba@gmail.com>
2019-08-06 14:04:01 +0100

    Revert "home page from and login page from zion"
    This reverts commit 4c1665c7ced7ba46d2952e61b0dfdf40bb424ac7.

--- ui/upload/success.php
+++ ui/upload/success.php
@@ -2,17 +2,16 @@
 
 	<div>
 		<p> 
-			<img>
-		 	Your product has been uploaded successfully
+		 Your product has been uploaded successfully
 		</p> 
 
 		<div>
 			<input type="text" dd_display="product_link" placeholder="xxxxx">
 		</div>
 
-		<a href="">View product <img> </a>
-		<a href=""> Add product <img> </a>
-	    <a href=""> See all product <img> </a>
+		<a href="">View product</a>
+		<a href=""> Add product</a>
+	    <a href=""> See all product</a>
 	</div>
 
 
