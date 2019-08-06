4c1665c7ced7ba46d2952e61b0dfdf40bb424ac7
mukorojeffrey <mukorojeffreyoghenevwegba@gmail.com>
2019-08-06 12:16:11 +0100

    home page from and login page from zion

--- ui/upload/success.php
+++ ui/upload/success.php
@@ -2,16 +2,17 @@
 
 	<div>
 		<p> 
-		 Your product has been uploaded successfully
+			<img>
+		 	Your product has been uploaded successfully
 		</p> 
 
 		<div>
 			<input type="text" dd_display="product_link" placeholder="xxxxx">
 		</div>
 
-		<a href="">View product</a>
-		<a href=""> Add product</a>
-	    <a href=""> See all product</a>
+		<a href="">View product <img> </a>
+		<a href=""> Add product <img> </a>
+	    <a href=""> See all product <img> </a>
 	</div>
 
 
